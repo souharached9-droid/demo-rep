900c8a619b0f49b1e0f292eb1aa33f34f8c39dd6
suzy <souharached9@gmail.com>
2026-06-08 09:50:31 +0200

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+void main() {
+  runApp(const MyApp());
+}
+ 
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+ 
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData.light(useMaterial3: true),
+      home: simpleproject(),
+    );
+  }
+}
+
+class simpleproject extends StatelessWidget {
+  const simpleproject({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
